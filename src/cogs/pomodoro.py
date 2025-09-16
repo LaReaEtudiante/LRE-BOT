b9df350eab217e75ee2efd41b9f9ccb230db0b90
LRE <lareaetudiante@gmail.com>
2025-09-16 13:47:25 +0200

    Update pomodoro.py

--- src/cogs/pomodoro.py
+++ src/cogs/pomodoro.py
@@ -1,1 +1,15 @@
+# ==========================
+# LRE-BOT/src/cogs/pomodoro.py
+# ==========================
+from discord.ext import commands
 
+class PomodoroCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="joina")
+    async def joina(self, ctx):
+        await ctx.send("⏳ Tu as rejoint le mode A (placeholder)")
+
+async def setup(bot):
+    await bot.add_cog(PomodoroCog(bot))
