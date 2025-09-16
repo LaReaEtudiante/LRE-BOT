56c5bae30d5aa663ca107134156aba188582ee96
LRE <lareaetudiante@gmail.com>
2025-09-16 13:48:11 +0200

    Update events.py

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -1,1 +1,15 @@
+# ==========================
+# LRE-BOT/src/cogs/events.py
+# ==========================
+from discord.ext import commands
 
+class EventsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.bot.user} est connecté et prêt !")
+
+async def setup(bot):
+    await bot.add_cog(EventsCog(bot))
