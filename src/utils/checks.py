534657471e5d2a4fe94abbebb1abce68e58e8b80
LRE <lareaetudiante@gmail.com>
2025-09-16 13:46:36 +0200

    Update checks.py

--- src/utils/checks.py
+++ src/utils/checks.py
@@ -1,1 +1,9 @@
+# ==========================
+# LRE-BOT/src/utils/checks.py
+# ==========================
+from discord.ext import commands
 
+def is_admin():
+    async def predicate(ctx):
+        return ctx.author.guild_permissions.administrator
+    return commands.check(predicate)
