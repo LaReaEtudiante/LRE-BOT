d97bf34913cb8aca0692708a5b094399b388c0c7
LRE <lareaetudiante@gmail.com>
2025-09-16 13:45:21 +0200

    Update config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,13 @@
+# ==========================
+# LRE-BOT/src/core/config.py
+# ==========================
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+DB_PATH = os.getenv("DB_PATH", "data/bot.db")
+RESET_HOUR = int(os.getenv("RESET_HOUR", 0))
+TIMEZONE = os.getenv("TIMEZONE", "Europe/Zurich")
 
