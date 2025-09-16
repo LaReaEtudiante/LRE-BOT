4b9d67a13859c31be0b64f8345887aabb70204a6
LRE <lareaetudiante@gmail.com>
2025-09-16 13:45:56 +0200

    Update logger.py

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,1 +1,11 @@
+# ==========================
+# LRE-BOT/src/core/logger.py
+# ==========================
+import logging
 
+logger = logging.getLogger("lre-bot")
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] [%(levelname)-8s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
