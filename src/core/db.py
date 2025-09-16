3ff6d7d3e2ef1d3508a33048e2580aa83d40dd74
LRE <lareaetudiante@gmail.com>
2025-09-16 13:45:41 +0200

    Update db.py

--- src/core/db.py
+++ src/core/db.py
@@ -1,1 +1,11 @@
+# ==========================
+# LRE-BOT/src/core/db.py
+# ==========================
+import aiosqlite
+from core import config
 
+async def init_db():
+    async with aiosqlite.connect(config.DB_PATH) as db:
+        with open("init_db.sql", "r", encoding="utf-8") as f:
+            await db.executescript(f.read())
+        await db.commit()
