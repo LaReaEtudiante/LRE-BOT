1d54b58170bb1d863e2f93babdbea9d3715867c5
LRE <lareaetudiante@gmail.com>
2025-10-07 10:27:23 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-# ==========================
-# LRE-BOT/src/main.py
-# ==========================
+# src/main.py
 import discord
 from discord.ext import commands
 from core import config
@@ -11,20 +9,14 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="*", help_command=None, intents=intents)
 
-# Charger les cogs au démarrage
-@bot.event
-async def setup_hook():
-    initial_cogs = [
-        "cogs.admin",
-        "cogs.user",
-        "cogs.pomodoro",
-        "cogs.events"
-    ]
+async def _setup_hook():
+    initial_cogs = ["cogs.admin", "cogs.user", "cogs.pomodoro", "cogs.events"]
     for cog in initial_cogs:
         await bot.load_extension(cog)
     print("[INFO] Tous les cogs ont été chargés avec succès.")
 
-# Lancer le bot
+bot.setup_hook = _setup_hook  # ← important
+
 if config.TOKEN is None:
     raise ValueError("❌ Le token Discord est introuvable. Vérifie ton fichier .env !")
 
